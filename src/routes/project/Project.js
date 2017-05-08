5205b087c4df187c16b3d42e35d0c2eb31874f13
justin leggett <justin@codingrookie.com>
2017-05-08 16:21:35 -0500

    working on background image showing through as th euser scrolls

--- src/routes/project/Project.js
+++ src/routes/project/Project.js
@@ -20,54 +20,64 @@ class Project extends React.Component {
     return (
         <div className={s.root}>
             <div className={s.container}>
-                <section>
-                    <div><h1>Front End Projects</h1></div>
-                    <article className={s.breaking}>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />>
-                        <h1>Washington D.C. Attacked By Flying Saucers</h1>
-                        <h2>Dateline Washington D.C.</h2>
-                        <h3>Frank Bragg reporting</h3>
-                        <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
-                    </article> 
-                    <article className={s.breaking}>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />>
-                        <h1>Washington D.C. Attacked By Flying Saucers</h1>
-                        <h2>Dateline Washington D.C.</h2>
-                        <h3>Frank Bragg reporting</h3>
-                        <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
-                    </article>
-                    <article className={s.breaking}>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />>
-                        <h1>Washington D.C. Attacked By Flying Saucers</h1>
-                        <h2>Dateline Washington D.C.</h2>
-                        <h3>Frank Bragg reporting</h3>
-                        <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
-                    </article>     
-                </section>    
-                <section>
-                    <div><h1>Back End Projects</h1></div>
-                    <article className={s.breaking}>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />>
-                        <h1>Washington D.C. Attacked By Flying Saucers</h1>
-                        <h2>Dateline Washington D.C.</h2>
-                        <h3>Frank Bragg reporting</h3>
-                        <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
-                    </article> 
-                    <article className={s.breaking}>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />>
-                        <h1>Washington D.C. Attacked By Flying Saucers</h1>
-                        <h2>Dateline Washington D.C.</h2>
-                        <h3>Frank Bragg reporting</h3>
-                        <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
-                    </article>
-                    <article className={s.breaking}>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />>
-                        <h1>Washington D.C. Attacked By Flying Saucers</h1>
-                        <h2>Dateline Washington D.C.</h2>
-                        <h3>Frank Bragg reporting</h3>
-                        <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
-                    </article>     
-                </section>            
+                <div className={s.flexContainer}>
+                    <h1 className={s.center}>Front End Projects</h1>
+                    <section>
+                        <article className={s.breaking}>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />
+                            <h1>TheBibleApp</h1>
+                            <h2>Dateline Washington D.C.</h2>
+                            <h3>Frank Bragg reporting</h3>
+                            <p>I struggled to get my children to listen to the bible being read to them(4 children under the age of 3),
+                               and this is the solution I came up with. I use simple animations(using ng-animate) to hold the childs 
+                               attention while the verses are read to them. With the click of a button the user can also be rerouted
+                               to an adult version of the app that uses the Digital Bible Platform API to pull back the King James 
+                               version of the bible. This project combines my knowledge of CSS, HTML, Javascript, Angular 1, Materialize,
+                               Digital Bible Platform API, and Firebase.</p>
+                        </article> 
+                        <article className={s.breaking}>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />
+                            <h1>Washington D.C. Attacked By Flying Saucers</h1>
+                            <h2>Dateline Washington D.C.</h2>
+                            <h3>Frank Bragg reporting</h3>
+                            <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
+                        </article>
+                        <article className={s.breaking}>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />
+                            <h1>Washington D.C. Attacked By Flying Saucers</h1>
+                            <h2>Dateline Washington D.C.</h2>
+                            <h3>Frank Bragg reporting</h3>
+                            <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
+                        </article>     
+                    </section>
+                </div>    
+                <div className={s.transparent}></div>
+                <div>
+                    <h1 className={s.center}>Back End Projects</h1>
+                    <section>
+                        <article className={s.breaking}>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />
+                            <h1>Washington D.C. Attacked By Flying Saucers</h1>
+                            <h2>Dateline Washington D.C.</h2>
+                            <h3>Frank Bragg reporting</h3>
+                            <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
+                        </article> 
+                        <article className={s.breaking}>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />
+                            <h1>Washington D.C. Attacked By Flying Saucers</h1>
+                            <h2>Dateline Washington D.C.</h2>
+                            <h3>Frank Bragg reporting</h3>
+                            <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
+                        </article>
+                        <article className={s.breaking}>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/earth-vs-the-flying-saucers.jpg" alt="Photograph of a flying saucer over the US Capitol building" />
+                            <h1>Washington D.C. Attacked By Flying Saucers</h1>
+                            <h2>Dateline Washington D.C.</h2>
+                            <h3>Frank Bragg reporting</h3>
+                            <p>The country was brought to a standstill today when flying saucers – presumably from Mars, although Venusians have also been suspected – appeared over the nation’s capital, intent on destruction. Curiously, they only attacked Pennsylvania Avenue, and have not appeared elsewhere in the country.</p>
+                        </article>     
+                    </section>
+                </div>               
             </div>
         </div>
     );
